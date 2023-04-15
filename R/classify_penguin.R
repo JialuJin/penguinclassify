86381e08937ab94521082a087483d36c0722c1eb
JialuJin <xyxxjinjialu@163.com>
2023-04-15 16:43:55 -0700

    added classify_penguin.R function

---
+++ R/classify_penguin.R
@@ -0,0 +1,11 @@
+classify_penguin <- function(bill_length, flipper_length) {
+  if (flipper_length > 205) {
+    return("Gentoo")
+  } else {
+    if (bill_length > 45) {
+      return("Chinstrap")
+    } else {
+      return("Adelle")
+    }
+  }
+}
